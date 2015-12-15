93cb71b5cab64ae092ec81f30184863665af1d6c
Mei Jun <ahumeijun@live.com>
2015-12-15 19:14:35 +0800

    Xcode Project init

---
+++ GeekWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeekWeather
+//
+//  Created by 梅俊 on 15/12/15.
+//  Copyright © 2015年 RangerStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
