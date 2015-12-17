2205f5d3b6a344fa5d872f2cf1f4f1edfde5f5d6
Mei Jun <ahumeijun@live.com>
2015-12-17 23:27:33 +0800

    command workpath property

--- GeekWeather/Commands/Command.swift
+++ GeekWeather/Commands/Command.swift
@@ -9,5 +9,17 @@
 import UIKit
 
 class Command: NSObject {
-
+    private(set) var workPath : String {
+        get {
+            return self.workPath
+        }
+        set(path) {
+            self.workPath = path
+        }
+    }
+    
+    init(path : String) {
+        super.init()
+        workPath = path
+    }
 }
