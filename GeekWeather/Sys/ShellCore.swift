b4804d5f10b9320078852228006fb77d2454d602
Mei Jun <ahumeijun@live.com>
2015-12-25 10:35:33 +0800

    execute command line string directly

--- GeekWeather/Sys/ShellCore.swift
+++ GeekWeather/Sys/ShellCore.swift
@@ -42,6 +42,35 @@ class ShellCore: NSObject, CommandDelegate {
     
     var delegate : ShellCoreDelegate?
     
+    func execute(string : String!) -> Bool {
+        var commandLine : CommandLine?
+        do {
+            commandLine = try CommandLineParser.shareParser.parse(string)
+        } catch {
+            self.delegate?.didReturnResult("command parse error.")
+            return false
+        }
+        
+        var command : Command?
+        do{
+            command = try CommandLineParser.shareParser.buildCommand(commandLine!)
+        } catch {
+            self.delegate?.didReturnResult("command build error.")
+            return false
+        }
+        
+        var result : String?
+        do {
+            result = try self.execute(command!)
+        } catch {
+            self.delegate?.didReturnResult("command execute error.")
+            return false
+        }
+        
+        self.delegate?.didReturnResult(result!)
+        return true
+    }
+    
     /**
      command execute
      the result will popup by ShellCoreDelegate
@@ -50,21 +79,15 @@ class ShellCore: NSObject, CommandDelegate {
      
      - returns: return true if execute cmd succeeded, otherwise false
      */
-    func execute(command : Command!) -> Bool {
-        
-        var result : String!
+    private func execute(command : Command!) throws -> String {
+        var result : String?
         do {
             command.delegate = self
             result = try command.execute()
-        } catch PathTree.PathTreeError.PathNotFound(let path) {
-            result = "\(path) not found"
         } catch {
-            result = "unknown error"
+            throw error
         }
-        
-        self.delegate?.didReturnResult(result)
-        
-        return true
+        return result!
     }
     
     var pathTree : PathTree {
